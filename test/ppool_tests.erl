7fa35406a33da141ee877823b8b4bff355138497
papachan <danloaiza@yahoo.com>
2015-11-02 07:08:29 -0500

    simple test

--- test/ppool_tests.erl
+++ test/ppool_tests.erl
@@ -1,2 +1,13 @@
 -module(ppool_tests).
--include_lib("eunit/include/eunit.hrl")
+-include_lib("eunit/include/eunit.hrl").
+
+-export([find_unique_name/0]).
+
+basic_test_() ->
+       fun () -> ?assert(1 + 1 =:= 2) end.
+
+find_unique_name() ->
+    ok = application:start(ppool),
+    Name = list_to_atom(lists:flatten(io_lib:format("~p",[erlang:timestamp()]))),
+    ?assertEqual(undefined, whereis(Name)),
+    Name.
