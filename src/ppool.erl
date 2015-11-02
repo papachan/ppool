802deaa759bc4248dd2eeb3cc7208caf897f0b86
papachan <danloaiza@yahoo.com>
2015-11-02 07:06:48 -0500

    adding application start method

--- src/ppool.erl
+++ src/ppool.erl
@@ -1,8 +1,16 @@
 %%% API module for the pool
 -module(ppool).
 
--export([start_link/0, stop/0, start_pool/3, 
+-behavior(application).
+
+-export([start/2, stop/1, start_pool/3,
          run/2, sync_queue/2, async_queue/2, stop_pool/1]).
+
+start(normal, _Args) ->
+    ppool_supersup:start_link().
+
+stop(_State) ->
+    ok.
 
 start_link() ->
     ppool_supersup:start_link().
